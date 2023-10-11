6c184dd5ce663c8274dddf96424749c84b578694
Anhela <anhelaf1@gmail.com>
2023-10-11 00:14:41 -0600

    Update main.c

--- main.c
+++ main.c
@@ -16,6 +16,7 @@
 #include <errno.h>
 #include "ChangeClk.h"
 #include "UART2.h"
+#include "Timer.h"
 #include "IO.h"
 
 //// CONFIGURATION BITS ////
@@ -64,6 +65,7 @@
 // GLOBAL VARIABLES
 unsigned int temp;
 unsigned int i;
+uint16_t ISR_flag; 
 
 // MACROS
 #define Nop() {__asm__ volatile ("nop");}
@@ -86,17 +88,18 @@ int main(void) {
 
     // LED on RB8
     TRISBbits.TRISB8 = 0;
-    
-    // Switch clock: 32 for 32kHz, 500 for 500 kHz, 8 for 8MHz 
-    NewClk(8); 
-    
+
+    // Configure push button pins (RA2, RA4, RB4) as inputs.
     configure_peripherals();
     CN_init();
 
-
    while(1)
     {
-    
+       Idle();
+       //if (ISR_flag == 1) {
+       //    ISR_flag = 0;
+          
+       //}
     }
    return 0;
 }
