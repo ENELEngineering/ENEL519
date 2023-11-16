87b6f2d78aee6ff12ffaf323edd6dfee5ff34d8c
Anhela <anhelaf1@gmail.com>
2023-11-15 17:33:05 -0700

    CVREF config

--- main.c
+++ main.c
@@ -92,6 +92,12 @@ int main(void) {
     TRISAbits.TRISA3 = 1; // Set RA3 as input.
     TRISAbits.TRISA6 = 0; // Set RA6 as output
     
+    // from CVREF project
+    TRISBbits.TRISB14 = 0; // Set to output.
+    CVRCONbits.CVREN = 1; // CVREF circuit is powered on.
+    CVRCONbits.CVROE = 1; // CVREF voltage level is output on CVREF pin.
+    CVRCONbits.CVRSS = 0; // Comparator reference source CVRSRC = AVDD - AVSS.
+    
     while(1) {
         ComparatorInit();
         //Idle();
