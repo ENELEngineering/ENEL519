24429ce1b7911901ba953d1371814f35839f86a6
Anhela <anhelaf1@gmail.com>
2023-11-30 17:54:27 -0700

    code refactor (separated ADC)

--- ZSense.c
+++ ZSense.c
@@ -9,6 +9,7 @@
 #include <math.h>
 #include "xc.h"
 #include "ZSense.h"
+#include "ADC.h"
 #include "Timer.h"
 #include "UART2.h"
 
@@ -36,84 +37,6 @@ void CTMUinit(uint8_t current_bits) {
     CTMUICONbits.ITRIM = 0b111110; // Nominal current output specified by IRNG<1:0>
     CTMUICONbits.IRNG = current_bits; // for 5.5 uA  
     return;
-}
-
-/*
- * Provide ADC settings configuration and initializations.
- * Set bits in AD1CON1 register.
- */
-void configure_ADC_AD1CON1(void) {
-    AD1CON1bits.ADON = 1; // A/D converter module is operating for current.
-    AD1CON1bits.ADSIDL = 0; // Continue module operation in idle mode.
-    AD1CON1bits.FORM = 0b00; // Data output format should be saved as an integer.
-    AD1CON1bits.SSRC = 0b111; // Clearing SAMP bit ends sampling and starts conversion.
-    AD1CON1bits.ASAM = 0; // Sampling begins when SAMP bit is set.
-    // Clear the A/D Conversion status bit.
-    AD1CON1bits.DONE = 1; // A/D conversion is done.
-    return;
-}
-
-/*
- * Provide ADC settings configuration and initializations.
- * Set bits in AD1CON2 register.
- */
-void configure_ADC_AD1CON2(void) {
-    AD1CON2bits.VCFG = 0b000; // Voltage reference configuration VR+ = AVdd and VR- = AVss.
-    AD1CON2bits.CSCNA = 0; // Do not scan inputs.
-    // Sample/convert sequences per interrupt selection bits.
-    AD1CON2bits.SMPI = 0b0000; // Interrupts at the completion of conversion for each sample/convert sequence.
-    AD1CON2bits.BUFM = 0; // Buffer configured as one 16-word buffer.
-    AD1CON2bits.ALTS = 0; // Always use MUX A input multiplexer settings.
-    return;
-}
-
-/*
- * Provide ADC settings configuration and initializations.
- * Set bits in AD1CON3 register.
- */
-void configure_ADC_AD1CON3(void) {
-    AD1CON3bits.ADRC = 0; // Clock derived from the system clock.
-    AD1CON3bits.SAMC = 0b11111; // Slowest sampling time = 31*2/fclk.
-    return;
-}
-
-/**
- * Perform ADC conversion at AN11
- * @return 10-bit ADC output
- */
-unsigned int do_ADC(void) {
-    uint16_t ADCvalue; // 16 bit register used to hold ADC converted digital output ADC1BUFO.
-    
-    /*---------- ADC INITIALIZATION ----------*/
-    configure_ADC_AD1CON1();
-    configure_ADC_AD1CON2();
-    // Configure the ADC's sample time by setting bits in AD1CON3.
-    // Ensure sample time is 1/10th of signal being sampled or as per application's speed and needs.
-    configure_ADC_AD1CON3();
-    
-    // Select and configure ADC input.
-    AD1CHSbits.CH0NA = 0; // Channel 0 negative input is VR-.
-    AD1CHSbits.CH0SA = 0b1011; // Channel 0 positive input is AN11.
-    AD1PCFGbits.PCFG11 = 0; // Pin configured in Analog mode; I/O port read disabled; A/D samples pin voltage.
-    AD1CSSLbits.CSSL11 = 0; // Analog channel omitted from input scan.
-    TRISBbits.TRISB13 = 1; // Analog pin to measure voltage is set as an input.
-    
-     /*---------- ADC SAMPLING AND CONVERSION - CAN BE A DIFFERENT FUNCTION ----------*/
-    AD1CON1bits.SAMP = 1; // A/D sample/hold amplifier is sampling input.
-    
-    while(AD1CON1bits.DONE==0) { 
-        __delay32(1600);
-    }
-    ADCvalue = ADC1BUF0; // ADC output is stored in ADC1BUF0 as this point.
-    
-    AD1CON1bits.SAMP = 0; // Stop sampling
-    AD1CON1bits.ADON=0; // Turn off ADC, ADC value stored in ADC1BUFO.
-    AD1CON1bits.DONE = 1; // A/D conversion is done.
-    
-    // Stop current
-    CTMUCONbits.EDG1STAT = 1; 
-    CTMUCONbits.EDG2STAT = 1; 
-    return (ADCvalue); // Returns 10 bit ADC output stored in ADC1BIFO to calling function.
 }
 
 /**
--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -51,17 +51,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=ChangeClk.c main.c UART2.c comparator.c ZSense.c Timer.c
+SOURCEFILES_QUOTED_IF_SPACED=ChangeClk.c main.c UART2.c comparator.c ZSense.c Timer.c ADC.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/ChangeClk.o ${OBJECTDIR}/main.o ${OBJECTDIR}/UART2.o ${OBJECTDIR}/comparator.o ${OBJECTDIR}/ZSense.o ${OBJECTDIR}/Timer.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/ChangeClk.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/UART2.o.d ${OBJECTDIR}/comparator.o.d ${OBJECTDIR}/ZSense.o.d ${OBJECTDIR}/Timer.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/ChangeClk.o ${OBJECTDIR}/main.o ${OBJECTDIR}/UART2.o ${OBJECTDIR}/comparator.o ${OBJECTDIR}/ZSense.o ${OBJECTDIR}/Timer.o ${OBJECTDIR}/ADC.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/ChangeClk.o.d ${OBJECTDIR}/main.o.d ${OBJECTDIR}/UART2.o.d ${OBJECTDIR}/comparator.o.d ${OBJECTDIR}/ZSense.o.d ${OBJECTDIR}/Timer.o.d ${OBJECTDIR}/ADC.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/ChangeClk.o ${OBJECTDIR}/main.o ${OBJECTDIR}/UART2.o ${OBJECTDIR}/comparator.o ${OBJECTDIR}/ZSense.o ${OBJECTDIR}/Timer.o
+OBJECTFILES=${OBJECTDIR}/ChangeClk.o ${OBJECTDIR}/main.o ${OBJECTDIR}/UART2.o ${OBJECTDIR}/comparator.o ${OBJECTDIR}/ZSense.o ${OBJECTDIR}/Timer.o ${OBJECTDIR}/ADC.o
 
 # Source Files
-SOURCEFILES=ChangeClk.c main.c UART2.c comparator.c ZSense.c Timer.c
+SOURCEFILES=ChangeClk.c main.c UART2.c comparator.c ZSense.c Timer.c ADC.c
 
 
 
@@ -89,78 +89,90 @@ MP_LINKER_FILE_OPTION=,--script=p24F16KA101.gld
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/ChangeClk.o: ChangeClk.c  .generated_files/flags/default/17b376a40413143bb8673e9cece646a0d13d38ec .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/ChangeClk.o: ChangeClk.c  .generated_files/flags/default/64bdcda48245fa81fa02904306df467f1553d962 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/ChangeClk.o.d 
 	@${RM} ${OBJECTDIR}/ChangeClk.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  ChangeClk.c  -o ${OBJECTDIR}/ChangeClk.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/ChangeClk.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/a31296b7d8edd55d888b1405bfac5368c370e406 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/bc97054c0a8fc7eac6d1e34e84559cfdd3971a06 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/UART2.o: UART2.c  .generated_files/flags/default/adf13a48ee3076f0a132a47fc14aa88afd3873c .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/UART2.o: UART2.c  .generated_files/flags/default/c0e263d5774514af37ff4498baf46f23fe3da533 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/UART2.o.d 
 	@${RM} ${OBJECTDIR}/UART2.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  UART2.c  -o ${OBJECTDIR}/UART2.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/UART2.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/comparator.o: comparator.c  .generated_files/flags/default/7fce56a4c6b362ff902b32bcbe54520263017dc6 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/comparator.o: comparator.c  .generated_files/flags/default/549c4197330f4c4f37ef23bfc95d02548b3cfb2f .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/comparator.o.d 
 	@${RM} ${OBJECTDIR}/comparator.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  comparator.c  -o ${OBJECTDIR}/comparator.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/comparator.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/ZSense.o: ZSense.c  .generated_files/flags/default/57e979aec661d16c0fc858786072f052430ee425 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/ZSense.o: ZSense.c  .generated_files/flags/default/a246a97625fad1731b648fa4a59eb5cada57cb6f .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/ZSense.o.d 
 	@${RM} ${OBJECTDIR}/ZSense.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  ZSense.c  -o ${OBJECTDIR}/ZSense.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/ZSense.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/Timer.o: Timer.c  .generated_files/flags/default/dfeea6534b73dfea943dd66b8b7c4920bfa8412e .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/Timer.o: Timer.c  .generated_files/flags/default/2b1af8894d97c3f2b2dec644fd25e2a7475d8a81 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/Timer.o.d 
 	@${RM} ${OBJECTDIR}/Timer.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  Timer.c  -o ${OBJECTDIR}/Timer.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/Timer.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
+${OBJECTDIR}/ADC.o: ADC.c  .generated_files/flags/default/92006b83b4f8684ed2f67c494df76fcd83f5b6f6 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/ADC.o.d 
+	@${RM} ${OBJECTDIR}/ADC.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ADC.c  -o ${OBJECTDIR}/ADC.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/ADC.o.d"      -g -D__DEBUG     -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
 else
-${OBJECTDIR}/ChangeClk.o: ChangeClk.c  .generated_files/flags/default/a5c0787bf182448248d0096ffd8aa2872a8eb969 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/ChangeClk.o: ChangeClk.c  .generated_files/flags/default/a3682496463d34649ba068be95daaa72bd660b8f .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/ChangeClk.o.d 
 	@${RM} ${OBJECTDIR}/ChangeClk.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  ChangeClk.c  -o ${OBJECTDIR}/ChangeClk.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/ChangeClk.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/527f17adf08a6d118a1d99b9f852e843320e1f11 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/main.o: main.c  .generated_files/flags/default/7528213449eddb74be1980222cbf8566d86ce180 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  main.c  -o ${OBJECTDIR}/main.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/UART2.o: UART2.c  .generated_files/flags/default/13dc2bbbd58eb4c3faad9bb8121928bbabc2ca3b .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/UART2.o: UART2.c  .generated_files/flags/default/e5abde3969330bd956abac764712a08f8b1facbd .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/UART2.o.d 
 	@${RM} ${OBJECTDIR}/UART2.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  UART2.c  -o ${OBJECTDIR}/UART2.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/UART2.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/comparator.o: comparator.c  .generated_files/flags/default/7ec7f659eb5f82499d15b2efb6c710fc00d739b6 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/comparator.o: comparator.c  .generated_files/flags/default/df719c5824ebd093eb5181094a56b12acd3e6021 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/comparator.o.d 
 	@${RM} ${OBJECTDIR}/comparator.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  comparator.c  -o ${OBJECTDIR}/comparator.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/comparator.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/ZSense.o: ZSense.c  .generated_files/flags/default/bdbff671c886d973c6122ea1a5024be6543ea7fb .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/ZSense.o: ZSense.c  .generated_files/flags/default/fc6b1e8acd1fdf14a2bd574a0db0515a87a8cb03 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/ZSense.o.d 
 	@${RM} ${OBJECTDIR}/ZSense.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  ZSense.c  -o ${OBJECTDIR}/ZSense.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/ZSense.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
-${OBJECTDIR}/Timer.o: Timer.c  .generated_files/flags/default/98e87a53fb4d4dc297d60c55a11dc3876a4e4658 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+${OBJECTDIR}/Timer.o: Timer.c  .generated_files/flags/default/915bf5b5605e03df5bf4f90dbbfd5227c330a262 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/Timer.o.d 
 	@${RM} ${OBJECTDIR}/Timer.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  Timer.c  -o ${OBJECTDIR}/Timer.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/Timer.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
+	
+${OBJECTDIR}/ADC.o: ADC.c  .generated_files/flags/default/7df03485a569e6e60e477989d9110b83af49b396 .generated_files/flags/default/da39a3ee5e6b4b0d3255bfef95601890afd80709
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/ADC.o.d 
+	@${RM} ${OBJECTDIR}/ADC.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  ADC.c  -o ${OBJECTDIR}/ADC.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/ADC.o.d"        -g -omf=elf -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -O0 -msmart-io=1 -Wall -msfr-warn=off    -mdfp="${DFP_DIR}/xc16"
 	
 endif
 
