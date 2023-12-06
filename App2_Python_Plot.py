d0cd09299827c6128aa8575bb738740d716ea94a
JohnVSantos <137139681+JohnMVSantos@users.noreply.github.com>
2023-12-06 16:35:58 -0700

    Added python plotting functionality

--- App2_Python_Plot.py
+++ App2_Python_Plot.py
@@ -18,8 +18,9 @@ import csv
 import serial  # pip install pyserial
 import time
 import pandas as pd
-#import plotly.express as px
+import plotly.express as px
 import decimal
+import matplotlib.pyplot as plt
 
 
 ## OPEN SERIAL PORT 
@@ -33,33 +34,37 @@ rxTimesList = []   #list to store time stamps of received uint16_t numbers
 startTime = time.time()   
 
 ## CAPTURE UART DATA
-while(time.time() - startTime < 0.5):  #record data for 1 sec
+limit = 15
+while(time.time() - startTime < limit):  #record data for 1 sec
+    
+    time_now = time.time() - startTime
+    print(f"Time {time_now} of {limit}", end="\r")
+
     line = ser.readline() # reads uint16_t nums as single bytes till \n n stores in string
     
-    if ((line != b' \n') and (line != b'\n')):
-        line = line.decode("utf-8")
-        line = line.replace("\r", "")
+    try:
+        line = line.strip()
+        line = line.rstrip()
+        line = rxNumsStr + line.decode('Ascii')
         line = line.replace("Capacitance:", "")
         line = line.replace("uF", "")
-        line = line.strip()
-        print(line)
+        line = line.replace("\x00", "")
         
-    #rxNumsStr = rxNumsStr + line.decode('Ascii')
+        capacitance = float(line)
+        rxNumsList.append(capacitance)
+
+        timeMeas = time.time() - startTime # Time stamp received number
+        rxTimesList.append(timeMeas) # save time stamps in a list
+
+    except UnicodeDecodeError:
+        continue
     
-   
-
-    # if ((line != b' \n') and (line != b'\n')) : #removes any '\n' without num captures
-    #     rxNumsStr = rxNumsStr + line.decode('Ascii')  # Converts string of received uint16_t num to ASCII and combines Rx nums into 1 string
-    #     timeMeas = time.time() -startTime # Time stamp received number
-    #     rxTimesList.append(timeMeas) #save time stamps in a list
-
 ## CLOSE SERIAL PORT    
 ser.close()  # close any open serial ports
 
-#rxStr = rxNumsStr #checks
-# print(rxStr)
-# print(rxNumsStr)  
-# print(rxTimesList)
+
+# plt.plot(np.array(rxTimesList), np.array(rxNumsList))
+# plt.show()
 
 
 # ### Rx DATA CLEANUP AND STRING TO FLOAT CONVERSION
@@ -68,7 +73,7 @@ ser.close()  # close any open serial ports
 # rxNumsList = rxNumsStr.split(' \n ')  # split string by \n n store in list
 # print(rxNumsList)
 # rxNumsList = [float(elem) for elem in rxNumsList]  # convert char in List into int
-# temp = decimal.Decimal(rxNumsList[1])
+temp = decimal.Decimal(rxNumsList[1])
 # if (abs(temp.as_tuple().exponent) > 12):
 #     unit = 'pF' # add logic to extract the correct unit (pF, nF, or uF)
 # elif (abs(temp.as_tuple().exponent) <= 12 & abs(temp.as_tuple().exponent) > 9):
@@ -78,25 +83,19 @@ ser.close()  # close any open serial ports
 # elif (abs(temp.as_tuple().exponent) <= 6 & abs(temp.as_tuple().exponent) > 3):
 #     unit = 'mF' # add logic to extract the correct unit (pF, nF, or uF)
 
-
-# # print(rxNumsList)       #check
-# print(len(rxTimesList))
-# print(len(rxNumsList))
-
+unit = "uF"
 
 # ### CONVERT Rx DATA INTO DATA FRAME
-# dF = pd.DataFrame()
-# dF['Time (sec)'] = rxTimesList
-# dF['Capacitance (f'{unit}')'] = rxNumsList
-
+dF = pd.DataFrame()
+dF['Time (sec)'] = rxTimesList
+dF["Capacitance (f'{unit}')"] = rxNumsList
 
 # ### DATA STATISTICS
-# print(dF.describe())
-
+print(dF.describe())
 
 # ### COPY RX DATA AND RX TIME IN CSV AND XLS FILES
-# dF.to_csv('RxDataFloat.csv', index = True)
+dF.to_csv('RxDataFloat.csv', index = True)
 
 # ### PLOT Rx DATA VS Rx TIME
-# fig = px.line(dF, x='Time (sec)', y='Capacitance (f'{unit}')', title = 'Measured Capacitance vs Time')
-# fig.show()
+fig = px.line(dF, x='Time (sec)', y="Capacitance (f'{unit}')", title = 'Measured Capacitance vs Time')
+fig.show()
